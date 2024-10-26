568a3dffced3b5c5e808ebf9ed22888becfbd0bd
rednerv <rednerv@163.com>
2024-10-26 23:56:48 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
@@ -11,10 +11,10 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.201/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Rednerv-Router/g' package/base-files/files/bin/config_generate
